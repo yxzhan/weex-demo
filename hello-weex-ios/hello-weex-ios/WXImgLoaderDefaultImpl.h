fb01fc136f697f19949198ea55a0e32b7f96a2a4
yanxiang.zyx <yanxiang.zyx@alibaba-inc.com>
2017-05-17 12:58:03 +0800

    delete pods dependencies

---
+++ hello-weex-ios/hello-weex-ios/WXImgLoaderDefaultImpl.h
@@ -0,0 +1,13 @@
+//
+//  WXImgLoaderDefaultImpl.h
+//  hello-weex-ios
+//
+//  Created by Yanxiang  on 2017/5/17.
+//  Copyright © 2017年 Yanxiang . All rights reserved.
+//
+
+#ifndef WXImgLoaderDefaultImpl_h
+#define WXImgLoaderDefaultImpl_h
+
+
+#endif /* WXImgLoaderDefaultImpl_h */
---
+++ hello-weex-ios/hello-weex-ios/WXImgLoaderDefaultImpl.m
@@ -0,0 +1,9 @@
+//
+//  WXImgLoaderDefaultImpl.m
+//  hello-weex-ios
+//
+//  Created by Yanxiang  on 2017/5/17.
+//  Copyright © 2017年 Yanxiang . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
